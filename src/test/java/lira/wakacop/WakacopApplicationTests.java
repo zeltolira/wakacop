19921f2a65dda53c37e6fde8e29193ddc05efd57
unknown <zelto@hotmail.com>
2023-11-06 06:43:42 -0300

    init

---
+++ src/test/java/lira/wakacop/WakacopApplicationTests.java
@@ -0,0 +1,13 @@
+package lira.wakacop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakacopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
